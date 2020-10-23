4eb20fc8a713df88f288b07be506b0f06244cd99
Shotaro Maruyama <testSample@aaa.com>
2020-10-23 22:32:10 +0900

    レビュー後

--- Assignment3/ViewController.swift
+++ Assignment3/ViewController.swift
@@ -8,70 +8,86 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     @IBOutlet weak var leftField: UITextField!
     @IBOutlet weak var rightField: UITextField!
-    
+
     @IBOutlet weak var leftLabel: UILabel!
     @IBOutlet weak var rightLabel: UILabel!
-    
+
     @IBOutlet weak var resultLabel: UILabel!
-    
+
     @IBOutlet weak var leftSwitch: UISwitch!
     @IBOutlet weak var rightSwitch: UISwitch!
+
     var leftNum:Double{
         get{
-            if(leftSwitch.isOn){
-                return -(Double(leftField.text ?? "0.0") ?? 0.0)
-            }
-            return Double(leftField.text ?? "0.0") ?? 0.0
+//            if(leftSwitch.isOn){
+//                return -(Double(leftField.text ?? "0.0") ?? 0.0)
+//            }
+//            return Double(leftField.text ?? "0.0") ?? 0.0
+
+            let originalNum = Double(leftField.text ?? "0.0") ?? 0.0
+            return leftSwitch.isOn ? -originalNum : originalNum
         }
-    }
-    var rightNum:Double{
-        get{
-            if(rightSwitch.isOn){
-                return -(Double(rightField.text ?? "0.0") ?? 0.0)
-            }
-            return Double(rightField.text ?? "0.0") ?? 0.0
-        }
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
     }
 
+    var rightNum:Double{
+        get{
+//            if(rightSwitch.isOn){
+//                return -(Double(rightField.text ?? "0.0") ?? 0.0)
+//            }
+//            return Double(rightField.text ?? "0.0") ?? 0.0
+            let originalNum = Double(rightField.text ?? "0.0") ?? 0.0
+            return rightSwitch.isOn ? -originalNum : originalNum
+        }
+    }
+
+    // 不要なので消しましょう
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//    }
+
     @IBAction func tapSumButton(_ sender: Any) {
-        let rightNum:Double = {
-            if(rightSwitch.isOn){
-                return -(Double(rightField.text ?? "0.0") ?? 0.0)
-            }
-            return Double(rightField.text ?? "0.0") ?? 0.0
-        }()
-        let leftNum:Double = {
-            if(leftSwitch.isOn){
-                return -(Double(leftField.text ?? "0.0") ?? 0.0)
-            }
-            return Double(leftField.text ?? "0.0") ?? 0.0
-        }()
+        // 以下は既にComputed Propertyがあるので要らないような？
+//        let rightNum:Double = {
+//            if(rightSwitch.isOn){
+//                return -(Double(rightField.text ?? "0.0") ?? 0.0)
+//            }
+//            return Double(rightField.text ?? "0.0") ?? 0.0
+//        }()
+//        let leftNum:Double = {
+//            if(leftSwitch.isOn){
+//                return -(Double(leftField.text ?? "0.0") ?? 0.0)
+//            }
+//            return Double(leftField.text ?? "0.0") ?? 0.0
+//        }()
+
         resultLabel.text = String(rightNum + leftNum)
     }
     @IBAction func switchRight(_ sender: UISwitch) {
-        rightLabel.text = String(rightNum)
+//        rightLabel.text = String(rightNum)
+        updateUI()
     }
+
     @IBAction func switchLeft(_ sender: UISwitch) {
-        leftLabel.text = String(leftNum)
+//        leftLabel.text = String(leftNum)
+        updateUI()
+    }
 
-    }
-    
     @IBAction func leftHasChanged(_ sender: UITextField) {
-        leftLabel.text = String(leftNum)
+//        leftLabel.text = String(leftNum)
+        updateUI()
     }
+
     @IBAction func rightHasChanged(_ sender: UITextField) {
+//        rightLabel.text = String(rightNum)
+        updateUI()
+    }
+
+    private func updateUI() {
+        leftLabel.text = String(leftNum)
         rightLabel.text = String(rightNum)
     }
-    
-    
 }
-
